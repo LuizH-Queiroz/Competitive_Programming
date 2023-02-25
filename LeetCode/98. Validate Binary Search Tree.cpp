9d358f4f468410fd8825bf42ea9dd88545b665be
Luiz Henrique Queiroz de Albuquerque Silva <83130948+LuizH-Queiroz@users.noreply.github.com>
2023-02-25 15:46:23 -0300

    98. Validate Binary Search Tree

---
+++ LeetCode/98. Validate Binary Search Tree.cpp
@@ -0,0 +1,31 @@
+/*
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left  subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root, long int minVal = LONG_MIN, long int maxVal = LONG_MAX) {
+        if (!root) return true;
+        else if (!(root->val > minVal && root->val < maxVal)) return false;
+
+        return isValidBST(root->left, minVal, root->val) && isValidBST(root->right, root->val, maxVal);
+    }
+};
