790074255fd340acaa3f1203a8cc4feccde0c910
LuizH-Queiroz <luizh.keiroz@gmail.com>
2023-03-02 11:15:40 -0300

    746. Min Cost Climbing Stairs

---
+++ LeetCode/746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,21 @@
+/*
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+*/
+
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+
+        for (int i = cost.size()-3; i >= 0; i--)
+        {
+            cost[i] += min(cost[i+1], cost[i+2]);
+        }
+        
+        return min(cost[0], cost[1]);
+    }
+};
