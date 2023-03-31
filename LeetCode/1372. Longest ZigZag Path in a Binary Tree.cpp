4c369d2217152a368f6e0e7baaadf6bef1e329aa
LuizH-Queiroz <luizh.keiroz@gmail.com>
2023-03-31 06:26:08 -0300

    1372. Longest ZigZag Path in a Binary Tree

---
+++ LeetCode/1372. Longest ZigZag Path in a Binary Tree.cpp
@@ -0,0 +1,54 @@
+/*
+You are given the root of a binary tree.
+
+A ZigZag path for a binary tree is defined as follow:
+
+    Choose any node in the binary tree and a direction (right or left).
+    If the current direction is right, move to the right child of the current node; otherwise, move to the left child.
+    Change the direction from right to left or from left to right.
+    Repeat the second and third steps until you can't move in the tree.
+
+Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
+
+Return the longest ZigZag path contained in that tree.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int longestZigZag(TreeNode* root) {
+        int longestPath = 0;
+
+        function<int(TreeNode*, bool)> dfs;
+        dfs = [&](TreeNode *node, bool leftDirection) {
+            if (!node)
+                return 0;
+
+
+            int leftLongest = dfs(node->left, false);
+            int rightLongest = dfs(node->right, true);
+            longestPath = max(longestPath, max(leftLongest, rightLongest));
+
+            if (leftDirection)
+                return 1 + leftLongest;
+            else
+                return 1 + rightLongest;
+        };
+
+
+        dfs(root, true); // It actually makes no difference if we pass 'true' or 'false' here,
+                         // as it only affects the return value
+        return longestPath;
+    }
+};
