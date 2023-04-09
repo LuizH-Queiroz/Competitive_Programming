3675d5adaea33259fea1cce6a45bf1f01b4168a0
LuizH-Queiroz <luizh.keiroz@gmail.com>
2023-04-09 09:18:01 -0300

    387. First Unique Character in a String

---
+++ LeetCode/387. First Unique Character in a String.cpp
@@ -0,0 +1,22 @@
+/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+*/
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int countCharacter[26] = {0};
+
+        for (int i = 0; i < s.size(); i++)
+            countCharacter[s[i] - 'a']++;
+        
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (countCharacter[s[i] - 'a'] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+};
