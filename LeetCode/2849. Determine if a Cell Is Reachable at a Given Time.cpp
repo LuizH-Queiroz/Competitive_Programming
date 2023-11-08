2f0806f25ca726f7263fac878e3945887d912934
Luiz Henrique Queiroz de Albuquerque Silva <83130948+LuizH-Queiroz@users.noreply.github.com>
2023-11-07 21:57:27 -0300

    2849. Determine if a Cell Is Reachable at a Given Time

---
+++ LeetCode/2849. Determine if a Cell Is Reachable at a Given Time.cpp
@@ -0,0 +1,24 @@
+/*
+You are given four integers sx, sy, fx, fy, and a non-negative integer t.
+
+In an infinite 2D grid, you start at the cell (sx, sy). Each second, you must move to any of its adjacent cells.
+
+Return true if you can reach cell (fx, fy) after exactly t seconds, or false otherwise.
+
+A cell's adjacent cells are the 8 cells around it that share at least one corner with it.
+You can visit the same cell several times.
+*/
+
+
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int xDiff = abs(sx - fx);
+        int yDiff = abs(sy - fy);
+
+        if (xDiff == 0 && yDiff == 0)
+            return t != 1;
+
+        return (min(xDiff, yDiff) + (max(xDiff, yDiff) - min(xDiff, yDiff))) <= t;
+    }
+};
