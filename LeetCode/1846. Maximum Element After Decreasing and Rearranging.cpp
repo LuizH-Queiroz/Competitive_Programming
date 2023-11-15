26b26ed2aef020d27bb8c4adc35654b37ee44f4d
Luiz Henrique Queiroz de Albuquerque Silva <83130948+LuizH-Queiroz@users.noreply.github.com>
2023-11-15 18:15:18 -0300

    1846. Maximum Element After Decreasing and Rearranging

---
+++ LeetCode/1846. Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,41 @@
+/*
+You are given an array of positive integers arr. Perform some operations (possibly none) on arr so that
+it satisfies these conditions:
+
+The value of the first element in arr must be 1.
+The absolute difference between any 2 adjacent elements must be less than or equal to 1. In other words,
+abs(arr[i] - arr[i - 1]) <= 1 for each i where 1 <= i < arr.length (0-indexed). abs(x) is the absolute value of x.
+There are 2 types of operations that you can perform any number of times:
+
+Decrease the value of any element of arr to a smaller positive integer.
+Rearrange the elements of arr to be in any order.
+Return the maximum possible value of an element in arr after performing the operations to satisfy the conditions.
+*/
+
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        vector<int> count(arr.size()+1, 0);
+        int maxVal = arr.size();
+        int extra = 0;
+
+        for (int i = 0; i < arr.size(); i++)
+            count[ min(arr[i], (int) arr.size()) ]++;
+
+        for (int i = arr.size(); i >= 1; i--)
+        {
+            if (count[i] == 0)
+            {
+                if (extra > 0) extra--;
+                else maxVal--;
+            }
+            else // count[i] > 0
+            {
+                extra += count[i] - 1;
+            }
+        }
+
+        return maxVal;
+    }
+};
